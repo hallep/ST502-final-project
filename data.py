a7c68a8d9e332992d79d3d68288dd7001bdddcaf
hallep <hpearce@unc.edu>
2026-04-14 17:05:38 -0400

    finalized regression variables.txt

--- data.py
+++ data.py
@@ -4,39 +4,38 @@ def get_xpt(name:str, cols:list[str]) -> pd.DataFrame:
     return pd.read_sas(f"https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2017/DataFiles/{name}_J.XPT", format="xport", index="SEQN")[cols]
 
 # Get columns from datasets
-xpts = dict(
-    demo = get_xpt("DEMO", ["RIAGENDR", "RIDAGEYR", "RIDRETH3", "DMDEDUC2", "DMDMARTL", "INDFMPIR"]),
-    alq = get_xpt("ALQ", ["ALQ111", "ALQ121"]),
-    dbq = get_xpt("DBQ", ["DBQ700"]),
-    paq = get_xpt("PAQ", ["PAQ605", "PAQ610", "PAD615", "PAQ620", "PAQ625", "PAD630", "PAQ650", "PAQ655", "PAD660", "PAQ665", "PAQ670", "PAD675", "PAD680"]),
-    slq = get_xpt("SLQ", ["SLD012", "SLD013"]),
-    bpq = get_xpt("BPQ", ["BPQ020"]),
-    cdq = get_xpt("CDQ", ["CDQ001"]),
-    diq = get_xpt("DIQ", ["DIQ010"]),
-    mcq = get_xpt("MCQ", ["MCQ160C", "MCQ160E", "MCQ160F", "MCQ160M", "MCQ160G", "MCQ160O", "MCQ160L"]),
-    bmx = get_xpt("BMX", ["BMXBMI"]),
-    tchol = get_xpt("TCHOL", ["LBXTC"]),
-    fertin = get_xpt("FERTIN", ["LBXFER"]),
-    folate = get_xpt("FOLATE", ["LBDRFO"]),
-    ins = get_xpt("INS", ["LBXIN", "LBDINLC"]),
-)
-
-# blood pressure
-bpx = get_xpt("BPX", ["BPXCHR", "BPXPLS", "BPXSY1", "BPXSY2", "BPXSY3", "BPXSY4", "BPXDI1", "BPXDI2", "BPXDI3", "BPXDI4"])
-bpx["BPXSY"] = bpx[["BPXSY1", "BPXSY2", "BPXSY3", "BPXSY4"]].median(axis=1, skipna=True)
-bpx["BPXDI"] = bpx[["BPXDI1", "BPXDI2", "BPXDI3", "BPXDI4"]].median(axis=1, skipna=True)
-xpts["bpx"] = bpx[["BPXCHR", "BPXPLS", "BPXSY", "BPXDI"]]
+xpts = [
+    get_xpt("DEMO", ["RIAGENDR", "RIDAGEYR", "RIDRETH3", "DMDEDUC2", "DMDMARTL", "INDFMPIR"]),
+    get_xpt("ALQ", ["ALQ111", "ALQ121"]),
+    get_xpt("BPX", ["BPXCHR", "BPXPLS", "BPXSY1", "BPXSY2", "BPXSY3", "BPXSY4", "BPXDI1", "BPXDI2", "BPXDI3", "BPXDI4"]),
+    get_xpt("DBQ", ["DBQ700"]),
+    get_xpt("PAQ", ["PAQ605", "PAQ610", "PAD615", "PAQ620", "PAQ625", "PAD630", "PAQ650", "PAQ655", "PAD660", "PAQ665", "PAQ670", "PAD675", "PAD680"]),
+    get_xpt("SLQ", ["SLD012", "SLD013"]),
+    get_xpt("BPQ", ["BPQ020"]),
+    get_xpt("CDQ", ["CDQ001"]),
+    get_xpt("DIQ", ["DIQ010"]),
+    get_xpt("MCQ", ["MCQ160C", "MCQ160E", "MCQ160F", "MCQ160M", "MCQ160G", "MCQ160O", "MCQ160L"]),
+    get_xpt("BMX", ["BMXBMI"]),
+    get_xpt("TCHOL", ["LBXTC"]),
+    get_xpt("FERTIN", ["LBXFER"]),
+    get_xpt("FOLATE", ["LBDRFO"]),
+    get_xpt("INS", ["LBXIN", "LBDINLC"]),
+]
 
 # Combine datasets
-data = pd.concat(xpts.values(), axis=1)
+data = pd.concat(xpts, axis=1)
 data.index = pd.Index(data.index.values.astype(int), name="SEQN")
-
-# remove subjects < 20 years old
-data = data[data["RIDAGEYR"] >= 20]
 
 # save raw data
 data.to_csv("NHANES_2017_data.txt", sep="\t")
 print(data)
+
+# blood pressure
+data["BPXSY"] = data[["BPXSY1", "BPXSY2", "BPXSY3", "BPXSY4"]].median(axis=1, skipna=True)
+data["BPXDI"] = data[["BPXDI1", "BPXDI2", "BPXDI3", "BPXDI4"]].median(axis=1, skipna=True)
+
+# remove subjects < 20 years old
+data = data[data["RIDAGEYR"] >= 20]
 
 # Clean data for regression
 variables = {
@@ -89,22 +88,22 @@ variables.update({
 
 # diet
 variables.update({
-    "diet2" : (data["DBQ700"] == 2).astype(int),
-    "diet3" : (data["DBQ700"] == 3).astype(int),
-    "diet4" : (data["DBQ700"] == 4).astype(int),
-    "diet5" : (data["DBQ700"] == 5).astype(int),
+    "excellentDiet" : (data["DBQ700"] == 1).astype(int),
+    "veryGoodDiet" : (data["DBQ700"] == 2).astype(int),
+    "goodDiet" : (data["DBQ700"] == 3).astype(int),
+    "fairDiet" : (data["DBQ700"] == 4).astype(int),
 })
 
 # physical activity
-def sort_pa(cBin:str, cDays:str, cMin:str) -> pd.Series:
+def sort_pa(cBin:str, cDays:str) -> pd.Series:
     return data[[cBin, cDays]].apply(lambda x: 0 if (x[cBin] == 2) else x[cDays], axis=1)
 
 variables.update({
-    "vigWork" : sort_pa("PAQ605", "PAQ610", "PAD615"),
-    "modWork" : sort_pa("PAQ620", "PAQ625", "PAD630"),
-    "vigRec" : sort_pa("PAQ650", "PAQ655", "PAD660"),
-    "modRec" : sort_pa("PAQ665", "PAQ670", "PAD675"),
-    "sedent" : data["PAD680"],
+    "vigWork" : sort_pa("PAQ605", "PAQ610"),
+    "modWork" : sort_pa("PAQ620", "PAQ625"),
+    "vigRec" : sort_pa("PAQ650", "PAQ655"),
+    "modRec" : sort_pa("PAQ665", "PAQ670"),
+    "sedentary" : data["PAD680"],
 })
 
 # numerical
