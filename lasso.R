66c088b3fee4e0ea8b32af23c91ba41dc3dd5cb2
Ko502 <kwilli32@ncsu.edu>
2026-04-25 12:13:22 -0400

    group lasso

--- lasso.R
+++ lasso.R
@@ -1,8 +1,13 @@
 library(glmnet)
 library(pROC)
+#install.packages("grpreg")
+library(grpreg)
 
 vars <- read.table("C:/Users/Ko Ago/Downloads/regression_variables (1).txt",
                    header = TRUE, sep = "\t", na.strings = "nan")
+
+# convert sedentary from minutes to hours
+vars$sedentary <- vars$sedentary / 60
 
 y <- as.numeric(vars$highBP)
 
@@ -26,9 +31,9 @@ y_train <- y[train_index]
 y_test  <- y[-train_index]
 
 lasso_model <- cv.glmnet(X_train, y_train,
-                         alpha = 1,          # <-- LASSO
+                         alpha = 1,
                          family = "binomial",
-                         nfolds = 5)         # <-- ADDED (5-fold CV)
+                         nfolds = 5)
 
 test_pred <- predict(lasso_model,
                      newx = X_test,
@@ -63,5 +68,43 @@ nonzero <- coefs[coefs != 0, , drop = FALSE]
 cat("\nNon-zero coefficients:\n")
 print(nonzero)
 
-cat("Lambda (min):", final_model$lambda.min, "\n")
-cat("Lambda (1se):", final_model$lambda.1se, "\n")
+cat("Lambda (min):", lasso_model$lambda.min, "\n")
+cat("Lambda (1se):", lasso_model$lambda.1se, "\n")
+
+# GROUP LASSO
+
+race_vars <- c("black", "hispanic", "asian", "otherrace", "mexAmer")
+
+group <- 1:ncol(X_train)
+col_names <- colnames(X_train)
+
+# assign same group to race variables
+race_group_id <- max(group) + 1
+group[col_names %in% race_vars] <- race_group_id
+
+# fit group lasso
+group_lasso_model <- cv.grpreg(X_train, y_train,
+                               group = group,
+                               family = "binomial",
+                               penalty = "grLasso")
+
+# predictions
+test_pred_grp <- predict(group_lasso_model,
+                         X_test,
+                         lambda = group_lasso_model$lambda.min,
+                         type = "response")
+
+# AUC
+roc_grp <- roc(y_test, as.vector(test_pred_grp))
+auc_grp <- auc(roc_grp)
+
+cat("Group LASSO Test AUC:", auc_grp, "\n")
+
+# plot ROC
+plot(roc_grp, main = "Group LASSO ROC Curve (Test Set)", col = "green", lwd = 2)
+
+# coefficients
+coefs_grp <- coef(group_lasso_model, lambda = group_lasso_model$lambda.min)
+
+cat("\nGroup LASSO Coefficients:\n")
+print(coefs_grp)
