1d92aae12d221b0bf6a9d6632157c96338cb25ed
hallep <hpearce@unc.edu>
2026-04-28 13:57:03 -0400

    Plotted PCR ROC curve and component composition heatmap

--- pcr.R
+++ pcr.R
@@ -42,15 +42,18 @@ pErr = ggplot(varY, aes(x = ncomps)) +
   geom_line(aes(y = RMSE.CV, color = "RMSE", linetype = "RMSE"), linewidth = 0.65) +
   geom_line(aes(y = RMSE.adjCV, color = "Adjusted RMSE", linetype = "Adjusted RMSE"), linewidth = 0.65) +
   labs(title = "Root Mean Square Error of Prediction", x = "Number of Components", y = "RMSE") +
-  scale_color_manual(name = "", breaks = c("RMSE", "Adjusted RMSE"), values = c("RMSE" = "black", "Adjusted RMSE" = "red")) + 
-  scale_linetype_manual(name = "", breaks = c("RMSE", "Adjusted RMSE"), values = c("RMSE" = "solid", "Adjusted RMSE" = "dashed")) +
-  theme_classic() + theme(legend.justification = c(1, 1), legend.position = c(0.9, 1),
+  scale_color_manual(name = "", breaks = c("RMSE", "Adjusted RMSE"), 
+                     values = c("RMSE" = "black", "Adjusted RMSE" = "red")) + 
+  scale_linetype_manual(name = "", breaks = c("RMSE", "Adjusted RMSE"), 
+                        values = c("RMSE" = "solid", "Adjusted RMSE" = "dashed")) +
+  theme_classic() + theme(legend.title = element_blank(), legend.justification = c(1, 1), legend.position = c(1, 1),
                           legend.background = element_rect(fill = "transparent", color = NA))
-  
+
 pR2 = ggplot(varY, aes(x = ncomps, y = R2)) + 
   geom_vline(xintercept = m, color="dimgrey", linewidth = 0.5, linetype = "dashed") + 
   geom_line(color = "mediumorchid", linewidth = 0.8) +
-  labs(title = bquote("Coefficient of Determination" ~ (R^2)), x = "Number of Components", y = expression(R^2)) +
+  labs(title = bquote("Coefficient of Determination" ~ (R^2)), 
+       x = "Number of Components", y = expression(R^2)) +
   theme_classic()
 
 # Components
@@ -61,40 +64,36 @@ varX = data.frame(
 )
 
 pX = ggplot(varX, aes(x = ncomps)) + 
-  geom_line(aes(y = exp, color = "Explained Variance"), linewidth = 0.8) +
-  geom_line(aes(y = cum, color = "Cumulative Variance"), linewidth = 0.8) +
+  geom_vline(xintercept = m, color="dimgrey", linewidth = 0.5, linetype = "dashed") + 
+  geom_line(aes(y = exp, color = "Explained Variance", linetype = "Explained Variance"), linewidth = 0.6) +
+  geom_line(aes(y = cum, color = "Cumulative Variance", linetype = "Cumulative Variance"), linewidth = 0.8) +
   scale_y_continuous(
     name = "Variance (%)",
     sec.axis = sec_axis(~.*16, name = "Cumulative Variance (%)")
   ) +
-  scale_color_manual(breaks = c("Explained Variance", "Cumulative Variance"), values = c("Explained Variance" = "black", "Cumulative Variance" = "red")) + 
-  labs(title = "Variance of Variables Explained by Components", x = "Component") +
+  scale_color_manual(name = "", breaks = c("Explained Variance", "Cumulative Variance"),
+                     values = c("Explained Variance" = "black", "Cumulative Variance" = "dodgerblue")) + 
+  scale_linetype_manual(name = "", breaks = c("Explained Variance", "Cumulative Variance"),
+                        values = c("Explained Variance" = "dashed", "Cumulative Variance" = "solid")) + 
+  labs(title = "Variance Explained by Components", x = "Component") +
   theme_classic() + theme(legend.title = element_blank(), legend.justification = c(0, 1), legend.position = c(0.1, 1),
                           legend.background = element_rect(fill = "transparent", color = NA))
 
+l = model$loadings[, 1:n]
 load = data.frame(
-  "Variable" = 1:(ncol(vars) - 1),
-  "c1" = model$loadings[, 1],
-  "c2" = model$loadings[, 2],
-  "c3" = model$loadings[, 3]
+  "variable" = factor(rep(rownames(l), ncol(l)), levels = as.vector(rownames(l))),
+  "component" = rep(1:n, each = nrow(l)),
+  "load" = stack(as.data.frame(l))$values
 )
 
-pLoad = ggplot(load, aes(x = Variable)) + 
-  geom_line(aes(y = c1, color = "Component 1"), linewidth = 0.6) +
-  geom_line(aes(y = c2, color = "Component 2"), linewidth = 0.6) +
-  geom_line(aes(y = c3, color = "Component 3"), linewidth = 0.6) +
-  labs(title = "Transformation of Variables into Components", x = "Variable", y = "Loading") +
-  scale_color_manual(name = "", values = c("Component 1" = "steelblue4", "Component 2" = "darkorange", "Component 3" = "green4")) + 
-  theme_classic() + theme(legend.justification = c(1, 0), legend.position = c(1, 0),
-                          legend.background = element_rect(fill = "transparent", color = NA))
-
-# Organize
-(pErr + pR2) / (pX + pLoad)
-
-# pErr
-# pR2
-# pX
-# pLoad
+pLoad = ggplot(load, aes(x = component, y = variable, fill = load)) + 
+  geom_tile() + 
+  scale_fill_gradient2(name = "Load", low = "steelblue3", mid = "white", high = "brown3", midpoint = 0,
+                       na.value = "grey85", guide = "colourbar", aesthetics = "fill") + 
+  labs(title = "Component Composition", x = "Component", y = "Variable") +
+  scale_x_continuous(expand = c(0, 0)) + scale_y_discrete(limits = rev) +
+  coord_fixed() +
+  theme_classic() + theme(plot.margin = margin(t = 0, r = 0, b = 0, l = 0))
 
 # ===== Predict ===== #
 
@@ -103,5 +102,24 @@ pred_pcr = predict(model, testData, ncomp=m)
 # ROC
 roc_pcr = roc(testData$highBP, pred_pcr)
 auc_pcr = auc(roc_pcr)
-plot(roc_pcr, main = "Principle Component Regression ROC Curve (Test Set)", col = "darkorange", lwd = 2)
 
+curve = data.frame(
+  "Specificity" = roc_pcr$specificities,
+  "Sensitivity" = roc_pcr$sensitivities
+)
+
+pROC = ggplot(curve, aes(x = Specificity, y = Sensitivity)) + 
+  geom_abline(intercept = 1, slope = 1, color = "gray", linewidth = 0.5) +
+  geom_line(color = "orange", linewidth = 0.8) + 
+  labs(title = "ROC Curve (Test Set)", x = "Specificity", y = "Sensitivity") +
+  scale_x_reverse() + theme_classic()
+
+# ===== Plots ===== #
+
+(pErr + pR2) / (pX + pROC)
+pLoad
+
+# pErr
+# pR2
+# pX
+# pROC
